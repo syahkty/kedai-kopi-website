fb7e85c8c9252feeb0d7bbde09b786d20b06f2de
Syahkty <andisyahktu46@gmail.com>
2024-01-03 12:37:08 +0800

    tambah metode pembayaran

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ document.addEventListener("alpine:init", () => {
     ],
   }));
 
-  Alpine.store('cart', {
+  Alpine.store("cart", {
     items: [],
     total: 0,
     quantity: 0,
@@ -21,7 +21,7 @@ document.addEventListener("alpine:init", () => {
       // jika belum ada
 
       if (!cartItem) {
-        this.items.push({...newItem, quantity: 1, total: newItem.price});
+        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
         this.quantity++;
         this.total += newItem.price;
       } else {
@@ -42,17 +42,17 @@ document.addEventListener("alpine:init", () => {
       }
     },
 
-    remove(id){
+    remove(id) {
       // ambil item yang ingin di remove berdasarkan id
       const cartItem = this.items.find((item) => item.id === id);
 
       // jika item lebih dari satu
-      if(cartItem.quantity > 1) {
+      if (cartItem.quantity > 1) {
         // telusuri satu satu
         this.items = this.items.map((item) => {
           // jika bukan barang yang di klik
-          if (item.id !== id){
-            return item
+          if (item.id !== id) {
+            return item;
           } else {
             item.quantity--;
             item.total = item.price * item.quantity;
@@ -60,14 +60,54 @@ document.addEventListener("alpine:init", () => {
             this.total -= item.price;
             return item;
           }
-        })
-      } else if (cartItem.quantity === 1){
-        this.items = this.items.filter((item) => item.id !== id );
-        this.quantity --
+        });
+      } else if (cartItem.quantity === 1) {
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
         this.total -= cartItem.price;
       }
-    }
+    },
   });
+});
+// form validate
+const checkoutButton = document.querySelector(".check-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length != 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
+// kirim data ketika checkout di klik
+checkoutButton.addEventListener("click", async function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+
+  // minta transaction token denga ajax
+  try {
+    const response = await fetch("php/placeOrder.php", {
+      method: "POST",
+      body: data,
+    });
+    const token = await response.text();
+    window.snap.pay(token);
+    // console.log(token);
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  
 });
 
 //  Koversi rupiah
